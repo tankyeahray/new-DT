e4e397a5f3d4460ecbf83cba4a41a5fd87df3bdd
tanyanrui <tanyanrui@qudian.com>
2017-07-18 16:25:48 +0800

    fix: 修改ESLint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
         "html"
     ],
     rules: {
-      
+      // 禁用import规则
+      'import/extensions': 0,
+      'import/first': 0,
+      'import/no-unresolved': 0,
+      'import/prefer-default-export': 0,
+      'import/no-extraneous-dependencies': 0,
 
       "comma-dangle": [2, "never"],
       "eol-last": 0
